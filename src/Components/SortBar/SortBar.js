0b5e758bbb5632096a2dbabe167a07ca08f4c1e9
dipen2000 <dipenchavda2000@gmail.com>
2022-06-07 12:50:43 +0530

    SortBar markup done

--- src/Components/SortBar/SortBar.js
+++ src/Components/SortBar/SortBar.js
@@ -1,13 +1,9 @@
 import "./SortBar.css";
 const SortBar = () => {
   return (
-    <div className="bord-3-green sortbar-container flex-row">
-      <div className="bord-3-green curs-point flex-row align-center-flex justify-center-flex sort-category">
-        Latest
-      </div>
-      <div className="bord-3-green curs-point flex-row align-center-flex justify-center-flex sort-category">
-        Oldest
-      </div>
+    <div className="bord-3-green sortbar-container flex-row align-center-flex justify-space-between-flex">
+      <h3>Trending Posts</h3>
+      <i className="fa-solid fa-sliders curs-point"></i>
     </div>
   );
 };
--- src/features/post/components/NewPost.js
+++ src/features/post/components/NewPost.js
@@ -2,9 +2,18 @@ import "../styles.css";
 import { Avatar } from "../../../Components/Avatar/Avatar";
 import { useSelector } from "react-redux";
 import { ButtonPrimary } from "../../../Components/Buttons";
+import { useState } from "react";
 
 const NewPost = () => {
   const loggedInUser = useSelector((state) => state.auth.user);
+  const [input, setInput] = useState({ text: "" });
+
+  const newPostInputHandler = (e) => {
+    const { name, value } = e.target;
+    setInput((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  console.log(input.text);
 
   return (
     <div className="new-post-container-grid bord-3-purple">
@@ -16,15 +25,20 @@ const NewPost = () => {
         />
       </div>
       <div className="bord-3-green">
-        <div className="flex-col gap-1">
-          <textarea
-            className="post-text-area"
-            placeholder="What's happening?"
-          />
-          <div className="flex-row justify-end-flex">
-            <ButtonPrimary>Post</ButtonPrimary>
+        <form>
+          <div className="flex-col gap-1">
+            <textarea
+              className="post-text-area"
+              name="text"
+              placeholder="What's happening?"
+              onChange={newPostInputHandler}
+              value={input.text}
+            />
+            <div className="flex-row justify-end-flex">
+              <ButtonPrimary disabled={!input.text.trim()}>Post</ButtonPrimary>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
